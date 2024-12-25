ff0ce4e003d1cff9e546df0e2114576de1024989
AnouarMantich <mantich0anouar@gmail.com>
2024-12-25 12:07:46 +0100

    app whit fault tolerance resilience4j

--- BILLING-SERVICE/src/main/java/org/example/billingservice/feigns/CustomerServiceClient.java
+++ BILLING-SERVICE/src/main/java/org/example/billingservice/feigns/CustomerServiceClient.java
@@ -1,19 +1,36 @@
 package org.example.billingservice.feigns;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.example.billingservice.models.Customer;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.hateoas.PagedModel;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import java.util.List;
+
 
 @FeignClient(name = "customer-service")
 public interface CustomerServiceClient {
 
     @GetMapping("/customers/{id}?projection=all")
+    @CircuitBreaker(name = "customerService" , fallbackMethod = "getDefaultCustomer")
     Customer getCustomerById(@PathVariable Long id);
 
     @GetMapping("/customers?projection=all")
+    @CircuitBreaker(name = "customerService" , fallbackMethod = "getDefaultCustomers")
     PagedModel<Customer> getCustomers();
+
+    default Customer  getDefaultCustomer(Long id,Exception exception) {
+        return Customer.builder()
+                .id(1L)
+                .name("Default Customer")
+                .email("default@email.com")
+                .build();
+    }
+
+    default List<Customer> getDefaultCustomers(Exception exception) {
+        return List.of();
+    }
 
 }
--- BILLING-SERVICE/src/main/java/org/example/billingservice/models/Customer.java
+++ BILLING-SERVICE/src/main/java/org/example/billingservice/models/Customer.java
@@ -1,8 +1,9 @@
 package org.example.billingservice.models;
 
+import lombok.Builder;
 import lombok.Data;
 
-@Data
+@Data @Builder
 public class Customer {
     private Long id;
     private String name;
